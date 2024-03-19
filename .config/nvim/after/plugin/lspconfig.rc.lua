3277ef1f9801cabfff456f38ed5ebfafba0d6639
Kanatsanan Janpakdee <kanasanan.j@mycloudfulfillment.com>
2024-03-19 20:06:38 +0700

    vim: lsp standardrb

--- .config/nvim/after/plugin/lspconfig.rc.lua
+++ .config/nvim/after/plugin/lspconfig.rc.lua
@@ -12,13 +12,91 @@ local on_attach = function(client, bufnr)
   end
 end
 
-nvim_lsp.solargraph.setup {
-}
+local cmd = vim.cmd
+local lsp = vim.lsp
+local map = vim.keymap.set
+local opt_local = vim.opt_local
+
+-- Do not load up plugin when in diff mode.
+if vim.opt.diff:get() then
+  cmd([[LspStop]])
+  return
+end
+
+local nvim_lsp = require("lspconfig")
+local nvim_lsp_windows = require("lspconfig.ui.windows")
+
+-- Custom on attach function.
+local lsp_on_attach = function()
+  -- Disable LSP for files larger than 100KB.
+  -- Mappings.
+  local opts = { buffer = true }
+  map("n", "ga", lsp.buf.code_action, opts)
+  map("n", "gd", lsp.buf.definition, opts)
+  map("n", "K", lsp.buf.hover, opts)
+  map("n", "gr", lsp.buf.references, opts)
+  map("n", "gR", lsp.buf.rename, opts)
+  map("i", "<C-k>", lsp.buf.signature_help, opts)
+
+  -- Fuzzy mappings using Telescope.
+  map("n", "<Space>lr", require("telescope.builtin").lsp_references)
+  map("n", "<Space>lw", require("telescope.builtin").lsp_document_symbols)
+  map("n", "<Space>lW", function()
+    require("telescope.builtin").lsp_workspace_symbols({
+      query = vim.fn.input("LSP Workspace Symbols❯ "),
+    })
+  end)
+  -- Note, LSP formatting will be handled by the conform.nvim plugin.
+
+  -- Disable Neovim LSP-set 'omnifunc' and 'formatexpr' options; these options
+  -- cause problems with my custom-completion mapping along with the 'gq'
+  -- command.
+  opt_local.omnifunc = ""
+  opt_local.formatexpr = ""
+end
+
+-- Global mappings.
+map("n", "'r", ":LspRestart<CR>", { silent = true })
+
+-- Add border to LSP windows such as `:LspInfo`.
+nvim_lsp_windows.default_options.border = "single"
+
+-- Custom on attach function which disables formatting where ALE will instead
+-- be used to format.
+local lsp_on_attach_no_formatting = function(client)
+  client.server_capabilities.documentFormattingProvider = false
+  client.server_capabilities.documentRangeFormattingProvider = false
+
+  lsp_on_attach()
+end
+
+-- Custom on attach function which disable LSP semantic highlighting.
+-- local lsp_on_attach_no_semantic_highlights = function(client)
+--   client.server_capabilities.semanticTokensProvider = nil
+-- end
+
+-- gem install solargraph
+-- solargraph clear
+-- solargraph download-core
+-- solargraph bundle
+nvim_lsp.solargraph.setup({
+  on_attach = lsp_on_attach_no_formatting, -- Use standardrb for formatting
+  capabilities = capabilities,
+  flags = { debounce_text_changes = 300 },
+  single_file_support = true, -- Allow LSP to work in standalone Ruby scripts
+  settings = { solargraph = { diagnostics = false } },
+})
+
+-- gem install standard
+nvim_lsp.standardrb.setup({
+  capabilities = capabilities,
+  flags = { debounce_text_changes = 300 },
+})
 
 nvim_lsp.gopls.setup {
   on_attach = on_attach,
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  cmd = { "/Users/kanatsanan.j/go/bin/gopls" }
+  cmd = { "/Users/kanatsananj/go/bin/gopls" }
 }
 
 nvim_lsp.tsserver.setup {
