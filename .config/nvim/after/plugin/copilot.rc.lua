91fb6a2a1bc3fcb0260ba00c29e177e3cbe2ca07
Kanatsanan Janpakdee <kanasanan.j@mycloudfulfillment.com>
2024-04-19 12:04:56 +0700

    vim: disable copilot

--- .config/nvim/after/plugin/copilot.rc.lua
+++ .config/nvim/after/plugin/copilot.rc.lua
@@ -4,3 +4,7 @@ local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept('<CR>')", { noremap = true, silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<C-j>", "<Plug>(copilot-next)", opts)
 vim.api.nvim_set_keymap("i", "<C-k>", "<Plug>(copilot-previous)", opts)
+
+vim.cmd([[
+  let g:copilot_enabled = v:false
+]])
