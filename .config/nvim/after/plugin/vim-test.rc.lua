baf346303727de2d0e643ca0cb7219f03102f948
Kanatsanan Janpakdee <kanasanan.j@mycloudfulfillment.com>
2024-11-30 17:17:56 +0700

    vim: combine vim-test strategy

--- .config/nvim/after/plugin/vim-test.rc.lua
+++ .config/nvim/after/plugin/vim-test.rc.lua
@@ -1,8 +1,5 @@
 vim.cmd([[
   let g:test#neovim#start_normal = 1
   let test#neovim#term_position = "bot 15"
-  let test#strategy = {
-      \ 'nearest': 'neovim',
-      \ 'file':    'neovim'
-      \}
+  let test#strategy = "neovim"
 ]])
